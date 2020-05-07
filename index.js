a756fe4861898a217db392f482360b235d8695ba
Nipesh <nipesh12pant@outlook.com>
2020-05-07 20:26:17 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// //Variable
+// //let vs var
+// let name = 'Nipesh';
+// console.log(name);
+
+// //Constant
+// //constant value variable
+
+// const intrestRate = 0.3;
+// //intrestRate = 1
+// console.log(intrestRate);
+
+
+// let isApproved = false;
+// let firstName = undefined;
+// let lastName = null;
+let person = {
+    name:'Mosh',
+    age: 30,
+};
+//Dot Notation
+person.name = 'Nipesh';
+
+//Bracket Notation
+person['age'] = 40;
+console.log(person);
